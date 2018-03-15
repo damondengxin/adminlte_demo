244f6613e7f6d461550e405290e88c0fb9601bcb
damondeng <304749970@qq.com>
2018-03-15 14:54:06 +0800

    基于adminlte修改,bower管理,后续只需添加app

---
+++ adminlte_demo/views.py
@@ -0,0 +1,18 @@
+import logging
+from django.shortcuts import redirect, render, HttpResponse
+from django.contrib.auth.decorators import login_required
+
+
+LOGGER = logging.getLogger('__name__')
+
+
+def index(request):
+    LOGGER.info("start application")
+    LOGGER.error("fail to start application")
+    #return redirect('/website/')
+    return render(request, 'index.html')
+
+
+@login_required()
+def noperm(request):
+    return render(request, 'noperm.html', {"user": request.user})
